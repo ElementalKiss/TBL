744c5f86f087b122126b83ed817c4f2fc28d54f3
reybe <reybe1118@gmail.com>
2019-09-04 00:26:17 +0900

    updated

---
+++ c/source/clang/ch1_total/main.c
@@ -0,0 +1,17 @@
+/*
+Title: ch01 total
+Function: print string
+FileName: main.c
+Edit Date: 2019.09.04
+Editor: reybe
+*/
+
+#include <stdio.h>
+
+
+int main(void)
+{
+	printf("Hello world! \n");
+	return 0;
+	// end of main function}
+}
