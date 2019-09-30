c4a7fc798ac29c2d417b585542e08542d20f9b18
reybe <reybe1118@gmail.com>
2019-09-30 22:55:31 +0900

    update

--- c/source/clang/ch04_total/main.c
+++ c/source/clang/ch04_total/main.c
@@ -22,6 +22,32 @@ int main(void)
 
     printf("0.1을 100번 더한 결과: %f \n", num5);
 
+    int num6 = 15;  // 00000000 00000000 00000000 00001111
+    int num7 = 20;  // 00000000 00000000 00000000 00010100
+    int num8 = num6 & num7; // num6과 num7의 비트단위 & 연산
+    printf("AND 연산의 결과: %d \n", num8);
 
+
+    int num9 = num6 | num7;
+    printf("OR 연산의 결과: %d \n", num9);
+
+    int num10 = num6 ^ num7;
+    printf("XOR 연산의 결과: %d \n", num10);
+
+    int num11 = ~ num6;
+    printf("NOT 연산의 결과: %d \n", num11);
+
+    int resurt1 = num6 << 1; //num6의 비트 열을 왼쪽으로 1칸씩 이동
+    int resurt2 = num6 << 2; //num6의 비트 열을 왼쪽으로 2칸씩 이동
+    int resurt3 = num6 << 3; //num6의 비트 열을 왼쪽으로 3칸씩 이동
+
+    printf("1칸 이동 결과: %d \n", resurt1);
+    printf("2칸 이동 결과: %d \n", resurt2);
+    printf("3칸 이동 결과: %d \n", resurt3);
+
+    int num12 = -16;    // 11111111 11111111 11111111 11110000
+    printf("2칸 오른쪽 이동의 결과: %d \n", num12 >> 2);
+    printf("3칸 오른쪽 이동의 결과: %d \n", num12 >> 3);
+    
     return 0;
 }
